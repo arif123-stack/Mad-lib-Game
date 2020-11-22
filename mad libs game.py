07350f52f4c01e07fa719dc5cacb6d4e9de8c739
arif123-stack <aa5736301@gmail.com>
2020-11-22 20:45:15 +0530

    first commit

---
+++ mad libs game.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", celebrity)
+
+
+# In[ ]:
+
+
+
+
